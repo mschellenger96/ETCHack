f3dac6e02b39f61e95c8aa5e72d75ddc6d53a248
Mitchell Schellenger <mschellenger@osisoft.com>
2018-08-28 16:27:08 -0700

    refresh

--- visualize.py
+++ visualize.py
@@ -1,5 +1,7 @@
 import cv2
 import vis_utils
+import time
+import random
 
 img = cv2.imread('BART_cc_map.png',1)
 cv2.namedWindow('BartMap')
@@ -7,27 +9,26 @@ cv2.setMouseCallback('BartMap', vis_utils.on_mouse)
 
 ###use this for updating times
 def render():
-    cv2.destroyAllWindows()
     global img
     img = cv2.imread('BART_cc_map.png',1)
     cv2.namedWindow('BartMap')
+    print_locations()
     cv2.imshow('BartMap',img)
-    print_times()
 
 def print_locations():
     font = cv2.FONT_HERSHEY_COMPLEX
     font_scale = 0.5
     font_color = (0, 0, 0)
     line_type = 1
-
     for key, value in vis_utils.stations.iteritems():
-        cv2.putText(img, key, value, font, font_scale, font_color, line_type)
+        cv2.putText(img, str(random.randint(1, 20)), value, font, font_scale, font_color, line_type)
 
 while(1):
-    cv2.imshow('BartMap',img)
-    print_locations()
+    render()
+    
     k = cv2.waitKey(20) & 0xFF
     if k == 27:
         break
+    time.sleep(10)
 
 cv2.destroyAllWindows()
