e27565fb0bc65a3ee4652495af2b576a62ead48f
Kane Penley <kpenley@osisoft.com>
2018-08-28 16:46:30 -0700

    bart client now writes data

--- bart-client.py
+++ bart-client.py
@@ -1,6 +1,11 @@
 import requests
 import json
 from data import Data
+from datetime import datetime
+import time as Time
+
+from SdsClient import SdsClient
+from SdsStream import SdsStream
 
 base = "http://api.bart.gov/api/sched.aspx?cmd=depart&orig=SANL&dest={0}&date=now&key=MW9S-E7SL-26DU-VV8V&b=0&a=1&l=1&json=y"
 stations = ["12th", "16th", "19th", "24th", "ashb", "antc", "balb", 
@@ -10,20 +15,35 @@ stations = ["12th", "16th", "19th", "24th", "ashb", "antc", "balb",
 "phil", "powl", "rich", "rock", "sbrn", "sfia", "shay", "ssan", "ucty", 
 "warm", "wcrk", "wdub", "woak"]
 
-for station in stations:
-    r = requests.get(base.format(station))
-    j = r.json()
-    trip = j["root"]["schedule"]["request"]["trip"]
+tenantID = "REPLACE"
+namespaceId = "REPLACE"
+clientId = "REPLACE"
+clientSecret = "REPLACE"
+resource = "REPLACE"
+address = "REPLACE"
+Authority = "REPLACE"
 
-    time = j["root"]["schedule"]["time"]
-    dest = trip["@destination"]
-    fare = float(trip["@fare"])
-    origTime = trip["@origTimeMin"]
-    destTime = trip["@destTimeMin"]
+client = SdsClient(tenantID, address, resource, Authority, clientId, clientSecret)
 
-    data = Data(time, dest, origTime, destTime, fare)
+while(True):
+    time = str(datetime.utcnow())
+    for station in stations:
+        r = requests.get(base.format(station))
+        j = r.json()
+        trip = j["root"]["schedule"]["request"]["trip"]
 
-    print data.toJson()
-    print
+        dest = trip["@destination"]
+        fare = float(trip["@fare"])
+        origTime = trip["@origTimeMin"]
+        destTime = trip["@destTimeMin"]
 
-    del data
+        data = Data(time, dest, origTime, destTime, fare)
+
+        try:
+            client.insertValue(namespaceId, station, data)
+            print "Wrote data to " + station
+        except Exception as e:
+            print "Failed to update for " + station + ": " + str(e)
+        del data
+    print "Done writing"
+    Time.sleep(60)
